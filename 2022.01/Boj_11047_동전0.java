61b96b019a17fe19fe795a14def710f109d079ed
Hwang seungyeon <syghkd@naver.com>
2022-02-01 17:46:04 +0900

    Boj 11047 동전 0

---
+++ 2022.01/Boj_11047_동전0.java
@@ -0,0 +1,36 @@
+package practice;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Boj_11047_동전0 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+		
+		int[] arr = new int[N];
+		for(int i=0;i<N;i++) {
+			arr[i]=Integer.parseInt(br.readLine());
+		}
+		int sum=0;
+		for(int i=N-1;i>=0;i--) {
+			if(arr[i]<=K) {
+				sum+= (K/arr[i]);
+				K = K-(arr[i]*(K/arr[i]));
+			}
+			if(K==0) {
+				break;
+			}
+		}
+		System.out.println(sum);
+
+	}
+
+}
