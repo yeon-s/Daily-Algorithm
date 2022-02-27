dbe8a9e23c12cf199e810c253267b45ead55ddb2
Hwang seungyeon <syghkd@naver.com>
2022-02-27 21:13:52 +0900

    Boj 1806 부분합

---
+++ 2022.02/Boj_1806_부분합.java
@@ -0,0 +1,47 @@
+package practice;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Boj_1806_부분합 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		int S = Integer.parseInt(st.nextToken());
+		
+		int[] arr = new int[N];
+		st = new StringTokenizer(br.readLine());
+		for(int i=0;i<N;i++) {
+			arr[i]=Integer.parseInt(st.nextToken());
+		}
+		int start=0;
+		int end=0;
+		int sum=arr[0];
+		int answer=N+1;
+		while(end<N && start<=end) {
+			
+			if(sum>=S) {
+				answer = Math.min(answer, end-start+1);
+				sum-=arr[start];
+				start++;
+			}else {
+				end++;
+				if(end<N) {
+					sum+=arr[end];
+				}
+			}
+		}
+		if(answer==N+1) {
+			System.out.println(0);
+		}else {
+			System.out.println(answer);			
+		}
+	}
+
+}
