6a5037178c2c855fcf64f2d3128e86e97d5f1cb7
Hwang seungyeon <syghkd@naver.com>
2022-04-03 02:37:04 +0900

    프로그래머스 자릿수 더하기

---
+++ 2022.04/Pgms_자릿수 더하기.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+
+        String str = n+"";
+        for(int i=0;i<str.length();i++){
+            answer+=str.charAt(i)-'0';
+        }
+
+        return answer;
+    }
+}
